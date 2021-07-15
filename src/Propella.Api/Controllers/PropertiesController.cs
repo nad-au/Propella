c61b59cc5d2e56bdabdbb8f114a31a8f566d93b1
Neil Dobson <neil.dobson71@gmail.com>
2021-07-16 09:32:50 +1000

    Project scaffolding

---
+++ src/Propella.Api/Controllers/PropertiesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Propella.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PropertiesController : ControllerBase
+    {
+        private readonly ILogger<PropertiesController> _logger;
+
+        public PropertiesController(ILogger<PropertiesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Property> Get()
+        {
+            return new List<Property>
+            {
+                new() {Address = "123 East St", Bedrooms = 2, BuildingType = "Home"},
+                new() {Address = "12/3 George St", Bedrooms = 1, BuildingType = "Unit"},
+            };
+        }
+    }
+}
---
+++ src/Propella.Api/Property.cs
@@ -0,0 +1,9 @@
+namespace Propella.Api
+{
+    public class Property
+    {
+        public string BuildingType { get; set; }
+        public string Address { get; set; }
+        public int Bedrooms { get; set; }
+    }
+}
