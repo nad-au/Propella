14e41a38f77006210c6fb07ef9dcf53f3eecc1a3
Neil Dobson <neil.dobson71@gmail.com>
2021-07-16 12:40:35 +1000

    Autofac integration

--- src/Propella.Api/Startup.cs
+++ src/Propella.Api/Startup.cs
@@ -1,9 +1,12 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Propella.Infrastructure.Modules;
 
 namespace Propella.Api
 {
@@ -15,6 +18,8 @@ namespace Propella.Api
         }
 
         public IConfiguration Configuration { get; }
+        
+        public ILifetimeScope AutofacContainer { get; private set; }
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -24,9 +29,16 @@ namespace Propella.Api
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "Propella.Api", Version = "v1"});
             });
         }
-
+        
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new ApplicationModule(Configuration));
+        }
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            AutofacContainer = app.ApplicationServices.GetAutofacRoot();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
