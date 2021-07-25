6818981d7f74923cb8d05ade7623a6edc055223f
Neil Dobson <neil.dobson71@gmail.com>
2021-07-25 17:34:51 +1000

    Use Swagger in release

--- src/Propella.Api/Startup.cs
+++ src/Propella.Api/Startup.cs
@@ -52,9 +52,10 @@ namespace Propella.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Propella.Api v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Propella.Api v1"));
 
             app.UseHttpsRedirection();
 
